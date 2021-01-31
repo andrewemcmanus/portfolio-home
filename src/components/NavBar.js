7a510a536630b02c82880910e5629c041f571b7c
Andrew McManus <andrewmcmanusmusic@gmail.com>
2021-01-31 10:38:18 -0500

    [material-ui] add material-ui

---
+++ src/components/NavBar.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { NavLink, Link } from 'react-router-dom';
+
+const Navbar = (props) => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className="container">
+                <Link className="navbar-brand" to="/">D3 Exoplanets</Link>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample07" aria-controls="#navbarsExample07" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarsExample07">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item">
+                            <NavLink className="nav-link" exact to="/">home</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link"  to="/about">about me</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link"  to="/projects">projects</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink = className="nav-link" to="/contact">contact</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <a href="https://www.dropbox.com/s/cr4jysq47ne7wy2/Andrew_McManus_Software_Engineer_1_2021.pdf?dl=0">download resume</a>
+                        </li>
+
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
