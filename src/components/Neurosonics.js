6d606b97c92023a42364321ecc3d8a90ce735d72
Andrew McManus <andrewmcmanusmusic@gmail.com>
2021-02-17 16:51:27 -0500

    add ircamverb viewer

--- src/components/Neurosonics.js
+++ src/components/Neurosonics.js
@@ -1,8 +1,11 @@
 import React from 'react';
-
+import ReactPlayer from 'react-player';
+// Vimeo link for moving sources:
 export default function Neurosonics() {
   return (
+    <div>
     <h3>Neurosonics</h3>
-
+    <ReactPlayer url="https://vimeo.com/513578770" />
+    </div>
   )
 }
